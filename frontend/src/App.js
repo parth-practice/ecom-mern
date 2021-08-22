0cd75eb1ab7b284e574fe9e14f8aeb04094a9042
Parth Patel <parth_patel@comcast.com>
2021-08-22 14:52:49 -0400

    adding header and footer compo and also bring react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <>
-      <h1>This is React Js Ecom site</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>This is React Js Ecom site</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+    return(
+        <Container>
+            <Row className="main">
+                <Col className="text-center py3">Copyright &copy; Parth's Ecom</Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Footer;
